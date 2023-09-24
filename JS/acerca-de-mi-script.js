4d135d6cc35e610b9f93ecf822d675c82cdc5be5
Maximiliem <maximiliem@yahoo.com>
2023-09-24 18:18:57 -0300

    Add: styles on acerca-de-mi
    buttons new positions & new ubication of image

---
+++ JS/acerca-de-mi-script.js
@@ -0,0 +1,10 @@
+/* 
+desde el DOM tengo que hacer aparecer esto
+<div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Título de la tarjeta</h5>
+          <p class="card-text">Un texto de ejemplo rápido para colocal cerca del título de la tarjeta y componer la mayor parte del contenido de la tarjeta.</p>
+          <a href="#" class="btn btn-primary">Ir a algún lugar</a>
+        </div>
+      </div> */
