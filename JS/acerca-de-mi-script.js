037fd67af5263cca4cf34771a2ecc892f75e6a30
Maximiliem <maximiliem@yahoo.com>
2023-09-24 21:54:22 -0300

    Add: biography information
    put new content into acerca-de-mi.html with DOM

--- JS/acerca-de-mi-script.js
+++ JS/acerca-de-mi-script.js
@@ -1,10 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+  function information() {
+    const info = `
+  <div class="info-container">
+    <div class="row align-items-md-stretch">
+
+    <div class="col-md-6">
+      <div class="imagen3">
+        <img class="maxDraw" src="../Images/maximiliem-image-removebg.png" alt="Dibujo del rostro de Maximiliem">
+      </div> 
+    </div> 
+      
+
+      <div class="col-md-6">
+        <div class="h-100 p-5 bg-light border rounded-3 text-end">
+          <h2>Maximiliano Bertta Borges</h2>
+          <p>Soy un gran apasionado por la programación y descubrí el gusto por esta área del conocimiento en 2021 cuando realicé una materia en la facultad donde nos enseñaron SQL. A paritr de allí me enamoré de manera ferviente de la programación y decidí inscirbirme en Jóvenes a Programar en 2023.</p>
+          <p>Me encanta aprender constantemente y buscar nuevos desafíos que me lleven a nuevos conocimientos.</p>
+          <p>En el botón de abajo te dejo mi perfil de GitHub para que veas mis proyectos.</p>
+          <button class="btn btn-outline-secondary" type="button" onclick="location.href='https://github.com/Maximiliem'">GitHub</button>
+        </div>
+      </div>
+    </div>
+  </div>
+    `;
+    const infoOnPage = document.getElementById("sobreMi");
+    infoOnPage.innerHTML = info;
+
+  };
+
+  information();
+
   function cardsContent() {
     const cards = `
     <!--FORMACIÓN ACADÉMICA-->
     <div class="cards-container">
-    <div class="card text-end" style="width: 18rem;" id="card1">
+    <div class="card text-end bg-light" style="width: 18rem;" id="card1">
     <img src="../Images/maxi-formacion-academica.jpeg"" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">Formación Académica</h5>
@@ -14,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
   </div>    
 
   <!--FORMACIÓN EN DESARROLLO WEB-->
-  <div class="card text-end" style="width: 18rem;">
+  <div class="card text-end bg-light" style="width: 18rem;">
     <img src="../Images/maxi-programacion.png" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">Formación en Desarrollo Web</h5>
@@ -24,7 +55,7 @@ document.addEventListener("DOMContentLoaded", function () {
   </div>  
 
   <!--BIOGRAFÍA-->
-  <div class="card text-end" style="width: 18rem;" id="card3">
+  <div class="card text-end bg-light" style="width: 18rem;" id="card3">
     <img src="../Images/maxi-biografia.jpg" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">Biografía</h5>
@@ -34,7 +65,7 @@ document.addEventListener("DOMContentLoaded", function () {
   </div>  
   </div>  
     `
-    ;
+      ;
 
     const cardsOnPage = document.getElementById("cardFooter");
 
